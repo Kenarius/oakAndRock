32c990cc7ac365d5ce3024e583b624ecb830ce4b
Ivan Okulevich <i.akulevich@trykitchenhub.com>
2026-04-21 20:26:34 +0300

    fix envs

--- common/conf/settings.py
+++ common/conf/settings.py
@@ -1,4 +1,6 @@
 """Common settings module."""
+import os
+
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
@@ -11,7 +13,7 @@ class PostgresSettings(BaseSettings):
     def db_uri(self) -> str:
         """Return database uri."""
 
-        SQLALCHEMY_DATABASE_URL = "postgresql://neondb_owner:npg_7Lq4WPbCGOes@ep-snowy-voice-agbqqvf6-pooler.c-2.eu-central-1.aws.neon.tech/neondb"
+        SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL", "0.0.0.0")
         ASYNC_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgresql://", "postgresql+asyncpg://")
         return ASYNC_DATABASE_URL
 
