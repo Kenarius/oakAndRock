5cce528946ba2e3b67f1016fc1894a00fab965f2
Ivan Okulevich <i.akulevich@trykitchenhub.com>
2026-05-10 23:52:51 +0300

    fix

--- src/repositories/blog.py
+++ src/repositories/blog.py
@@ -1,5 +1,7 @@
 """Blog repository."""
+from typing import Union, Sequence, Any
 
+from sqlalchemy import Row, RowMapping, select, desc
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from common.db.models.blog import Blog
@@ -14,3 +16,11 @@ class BlogRepository(BaseRepository):
     def __init__(self, session: AsyncSession) -> None:
         """Init Repository session."""
         super().__init__(session)
+
+    async def list(self, filters: Union[tuple, None] = None) -> Sequence[Row | RowMapping | Any]:
+        """Get list of filtered objects."""
+        query = select(self.model).order_by(desc(self.model.created_at))
+        if filters is not None:
+            query.filter(*filters)
+        objects = await self._session.execute(query)
+        return objects.scalars().all()
